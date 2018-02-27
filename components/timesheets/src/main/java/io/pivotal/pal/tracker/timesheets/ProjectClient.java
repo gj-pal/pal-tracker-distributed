a1b952a86203a5ce33b17cd530d3a69fbdddc94d
pal_user <newry1979@gmail.com>
2018-02-27 10:17:54 -0800

    Added circuit breaker

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,11 +1,20 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String endpoint;
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private Map<Long, ProjectInfo> projectMapCache = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
@@ -13,6 +22,14 @@ public class ProjectClient {
     }
 
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectMapCache.put(projectId, projectInfo);
+        return projectInfo;
+    }
+
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
+    public ProjectInfo getProjectFromCache(long projectId){
+        logger.info("Getting project with id {} from cache", projectId);
+        return projectMapCache.get(projectId);
     }
 }
